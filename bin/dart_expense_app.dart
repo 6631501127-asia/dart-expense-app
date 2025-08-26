6d5ec810c05329288b1721dbf82ec92f2e63bca9
asia <6631501127@lamduan.mfu.ac.th>
2025-08-26 19:48:24 +0700

    Complete two features: showall and totalPaid

--- bin/dart_expense_app.dart
+++ bin/dart_expense_app.dart
@@ -1,7 +1,10 @@
-void showAll() {}
-
-void totalPaid() {}
-
+void showAll() {
+  print('1. Lunch: 50 Baht');
+  print('2. Coffee: 70 Baht');
+}
+void totalPaid() {
+  print('Total = 120 Baht');
+}
 void main() {
   showAll();
 
